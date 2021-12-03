2b0715d27589e12fce2e8deef254fcacc30df83f
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2021-12-04 02:48:12 +0800

    ooh vue files

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# skep-website
+# skep-vue
 
-My site uses the usual three, but using the CSS pre-processor SCSS. And it's a complete abomination lol
+bruh
---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <skepTopHeader></skepTopHeader>
+</template>
+
+<script>
+import skepTopHeader from './layouts/SkepHeader.vue'
+
+export default {
+  name: 'Fat boi',
+  components: {
+    skepTopHeader
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
