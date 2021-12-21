09b685cea05f8234d22754332602617b4789964c
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2021-12-21 20:23:07 +0800

    Imported Font Awesome

--- src/main.js
+++ src/main.js
@@ -2,6 +2,20 @@ import { createApp } from "vue"
 import { createRouter, createWebHistory } from "vue-router"
 import App from "./App.vue"
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+library.add(fas);
+import { fab } from "@fortawesome/free-brands-svg-icons";
+library.add(fab);
+import { far } from "@fortawesome/free-regular-svg-icons";
+library.add(far);
+import { dom } from "@fortawesome/fontawesome-svg-core";
+dom.watch();
+
+const app = createApp(App);
+app.component("font-awesome-icon", FontAwesomeIcon);
+
 import Home from "@/pages/Home.vue"
 import About from "@/pages/About.vue"
 import Discography from "@/pages/Discography.vue"
