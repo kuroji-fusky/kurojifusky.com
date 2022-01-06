53b3074ab1f755da5952f684cf9ff1809e15ad3b
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-01-06 08:03:16 +0800

    I literally have no idea what I'm doing help

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'vue deploy'
 
-git push -f git@github.com:skepfusky/skepfusky.net.git main:gh-pages
+git push -f git@github.com:skepfusky/skepfusky-net.git main:gh-pages
 
 cd -
