07f3b633d74b9e6385c4c79db82c3982887dd76e
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-01-11 08:47:29 +0800

    yes

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <main>
+    <div id="avatar-flip-container">
+      
+    </div>
+  </main>
+  <footer>&copy; 2014-{{ new Date().getFullYear() }} Skepfusky</footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+  }
+});
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/mixins.scss';
+@import '@/assets/scss/defaults.scss';
+@import '@/assets/scss/styles.scss';
+</style>
