746e5bec883ad6e8a0e1e6c3672fc13d96a7cbdf
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-01-23 17:20:23 +0800

    I'm blushing right now lol

--- src/components/Fursona.vue
+++ src/components/Fursona.vue
@@ -1,12 +1,63 @@
 <template>
   <article>
     <h2>Fursona<span id="gradient-block"></span></h2>
-    <p>TBA</p>
+    <h3>Description</h3>
+    <p>
+      Skep is a fox and husky hybrid or fusky and was originally a husky. Propped with thick black
+      glasses and wearing a purple shirt with white outlines, and completely pantless lmao
+    </p>
+    <br>
+    <p>
+      Mixing with a typical traits both of a fox and a husky, he is a wholesome, outgoing, yet weird personality and he easily gets soft when he's flattered and
+      starts to blush lol
+    </p>
   </article>
-  
+  <article>
+    <h3>Color palette</h3>
+    <div id="gallery-layout">
+      <div class="palette" style="background: #2076c8">
+        <big>Blue fur</big>
+        <span>#2076c8</span>
+      </div>
+      <div class="palette" style="background: #48ddf7; color: #000">
+        <big>Cyan highlights</big>
+        <span>#48ddf7</span>
+      </div>
+      <div class="palette" style="background: #f8d817; color: #000">
+        <big>Yellow floof</big>
+        <span>#f8d817</span>
+      </div>
+      <div class="palette" style="background: #bb3eed">
+        <big>Eyes</big>
+        <span>#bb3eed</span>
+      </div>
+      <div class="palette" style="background: linear-gradient(to bottom, #bb3eed, #ffabff); color: #000">
+        <big>Eyes (Gradient)</big>
+        <span>#bb3eed</span>
+        <span>#ffabff</span>
+      </div>
+      <div class="palette" style="background: #fce392; color: #000">
+        <big>Bridge</big>
+        <span>#fce392</span>
+      </div>
+      <div class="palette" style="background: #6f3a0f">
+        <big>Nose</big>
+        <span>#6f3a0f</span>
+      </div>
+      <div class="palette" style="background: #b266ed">
+        <big>Purple shirt</big>
+        <span>#b266ed</span>
+      </div>
+      
+    </div>
+  </article>
   <article>
     <h3>Sketches/other versions</h3>
     <div id="gallery-layout">
+      <figure>
+        <img src="../assets/img/drawings-self/Jan2022Rough.png" alt="Was learning TypeScript in the middle of drawing this adorable boi">
+        <figcaption>Rough draft, cropped, no shading and no glasses</figcaption>
+      </figure>
       <figure>
         <img src="../assets/img/drawings-self/Jan2022Sketch.png" alt="Was learning TypeScript in the middle of drawing this adorable boi">
         <figcaption>Warmup Sketch #2; January 2022<br> <i style="opacity: .5">Was learning TypeScript in the middle of drawing this adorable boi</i></figcaption>
@@ -86,6 +137,33 @@
 </template>
 
 <style lang="scss">
+.palette {
+  --pl-size: 140px;
+
+  @include tablet-devices {
+    --pl-size: 120px;
+    font-size: 90%;
+  }
+
+  @include phone-devices {
+    height: 80px;
+    width: 100%;
+  }
+
+  border-radius: 6px;
+  height: var(--pl-size);
+  width: var(--pl-size);
+  border: 2px solid gray;
+  padding: 0.5em;
+  display: flex;
+  justify-content: flex-end;
+  flex-direction: column;
+
+  big {
+    font-weight: bold;
+  }
+}
+
 #gallery-layout {
   display: flex;
   flex-wrap: wrap;
--- src/components/SkepFooter.vue
+++ src/components/SkepFooter.vue
@@ -1,7 +1,9 @@
 <template>
   <footer>
-    <span style="font-size: 110%">&copy; {{ new Date().getFullYear() }} skepfusky</span>
-    <span style="font-size: 88%">Updated January 20, 2022</span>
-    <span style="font-size: 88%">Made in Vue 3 + Vite</span>
+    <div id="wrapper">
+      <span style="font-size: 110%">&copy; {{ new Date().getFullYear() }} skepfusky</span>
+      <span style="font-size: 88%">Updated January 20, 2022</span>
+      <span style="font-size: 88%">Made in Vue 3 + Vite</span>
+    </div>
   </footer>
 </template>
