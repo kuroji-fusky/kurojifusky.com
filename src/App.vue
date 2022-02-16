b63542ff53966ceaff5096bdf6179929b1092bf2
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-02-16 08:33:10 +0800

    moving fonts lol

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,8 @@ import TheFooter from "@/components/static/TheFooter.vue"
 </template>
 
 <style lang="scss">
+@import 'https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400&family=Noto+Sans+KR:wght@100;300;500;700&Roboto+Mono:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap';
+
 *,
 *::before,
 *::after {
