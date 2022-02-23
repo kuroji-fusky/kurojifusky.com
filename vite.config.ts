6a76a45b6029d618e30ae9673a0d42fa20a7ea22
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-02-23 14:04:49 +0800

    code spliting lol

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,23 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  build: {
+    chunkSizeWarningLimit: 1600,
+    target: 'es2015',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // This piece of shit is yelling at me
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
+  plugins: [vue()],
+});
