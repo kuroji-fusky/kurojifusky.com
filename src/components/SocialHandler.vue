ca6446d4d05bdb277c06e0fac549462763770973
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-02-28 13:33:30 +0800

    fixed steam link

--- src/components/SocialHandler.vue
+++ src/components/SocialHandler.vue
@@ -4,7 +4,7 @@
     <a href="https://www.twitter.com/skepfuskyjs" target="_blank"><i class="fab fa-twitter"></i></a>
     <a href="https://www.instagram.com/skepfuskyjs" target="_blank"><i class="fab fa-instagram"></i></a>
     <a href="https://www.github.com/skepfusky" target="_blank"><i class="fab fa-github"></i></a>
-    <a href="https://www.steamcommunity.com/skepfusky" target="_blank"><i class="fab fa-steam"></i></a>
+    <a href="https://steamcommunity.com/id/kokoro_husky/" target="_blank"><i class="fab fa-steam"></i></a>
     <a href="https://www.soundcloud.com/kokoro-husky-mixtapes" target="_blank"><i class="fab fa-soundcloud"></i></a>
     <a href="https://open.spotify.com/artist/3fouosCOFa1ykd6j9DZkWl?si=byCWjlfqR3OXDiGTiuCpqA" target="_blank"><i class="fab fa-spotify"></i></a>
     <a href="https://music.apple.com/us/artist/kokoro-husky/1521326000" target="_blank"><i class="fab fa-itunes-note"></i></a>
