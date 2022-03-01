fe5a3b1cbd7d5fa291dc2cc51df18d65c685d116
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-03-02 01:05:55 +0800

    fixed `resolve.alias` on vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,9 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
-    alias: [
-      { find: '@', replacement: path.resolve(__dirname, './src') },
-    ],
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
   build: {
     chunkSizeWarningLimit: 1600,
