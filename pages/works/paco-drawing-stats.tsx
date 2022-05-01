45bc25ab37069ec959999db31b7ffe9ac5221e15
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-05-01 19:34:52 +0800

    Added descs for my shitty projects

--- pages/works/paco-drawing-stats.tsx
+++
@@ -1,9 +0,0 @@
-import UnderConstruction from "../../components/UnderConstruction";
-
-export default function PacoDrawingStats() {
-  return(
-    <div>
-      <UnderConstruction />
-    </div>
-  )
-};
