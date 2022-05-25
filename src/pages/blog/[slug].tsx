09dc3bf399d3b85a561353ceebdbd352c24f49c2
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-05-25 13:06:42 +0800

    added static blog lmao

---
+++ src/pages/blog/[slug].tsx
@@ -0,0 +1,55 @@
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next";
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import { serialize } from "next-mdx-remote/serialize";
+import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
+import SEOHead from "@/components/SEOHead";
+import RoadmapContainer, { RoadmapItem } from "@/components/RoadmapContainer";
+
+const components = {
+	SEOHead,
+  RoadmapContainer,
+  RoadmapItem
+};
+
+export default function ShitBlogSlug({
+	source
+}: {
+	source: MDXRemoteSerializeResult<Record<string, unknown>>;
+}): InferGetStaticPropsType<typeof getStaticProps> {
+	return (
+		<div className="px-6 rendered-static-content">
+			<MDXRemote {...source} components={components} />
+		</div>
+	);
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+	const articlePath = path.join("content/blog");
+	const files = fs.readdirSync(articlePath);
+	const paths = files.map((fileName: string) => ({
+		params: { slug: fileName.replace(".mdx", "") }
+	}));
+
+	return {
+		paths,
+		fallback: false
+	};
+};
+
+type Params = {
+	[param: string]: any;
+};
+
+export const getStaticProps: GetStaticProps<Params> = async ({
+	params: { slug }
+}: Params) => {
+	const articlePath: any = fs.readFileSync(
+		path.join("content/blog", `${slug}.mdx`)
+	);
+
+	const { data: metaData, content } = matter(articlePath);
+	const mdxSrc = await serialize(content, { scope: metaData });
+	return { props: { source: mdxSrc } };
+};
