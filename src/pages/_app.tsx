5ca493214971706d3b75b184db21f50890639562
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-05-30 15:25:47 +0800

    I broke something again & I fixed it again haha

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import type { AppProps } from "next/app";
-import Layout from "@/components/Layout";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@/styles/globals.scss";
 import "@fortawesome/fontawesome-svg-core/styles.css";
