a90a73617e220f9f316cedfc1036bab48b094e4b
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-06-12 10:02:15 +0800

    Better way to remove `console.*`

--- next.config.js
+++ next.config.js
@@ -2,10 +2,14 @@
 const nextConfig = {
 	reactStrictMode: true,
 	compress: true,
+  swcMinify: true,
   trailingSlash: true,
 	images: {
 		domains: ["img.youtube.com"]
-	}
+	},
+  compiler: {
+    removeConsole: true,
+  }
 }
 
 module.exports = nextConfig
