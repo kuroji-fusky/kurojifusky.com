30e6a6570b4c07b336fa11cb32083ae2e1c39008
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-07-12 15:16:17 +0800

    Refactoring Python code

--- post-processing/index-meta.py
+++ post-processing/index-meta.py
@@ -6,7 +6,7 @@ Written by skepfusky
 """
 import argparse
 import numpy as np
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw
 
 # Parser stuff
 parser = argparse.ArgumentParser(
@@ -14,21 +14,20 @@ parser = argparse.ArgumentParser(
 parser.add_argument("-o", "--show-output", action="store_true", help="Show the output image")
 args = parser.parse_args()
 
-
 # Generate a simple, dark af gradient
-def gradient2d(start, stop, width, height, is_horizontal):
-  if is_horizontal:
-    return np.tile(np.linspace(start, stop, width), (height, 1))
-  else:
-    return np.tile(np.linspace(start, stop, height), (width, 1)).T
+def gradient2d(start, stop, width, height):
+  return np.tile(np.linspace(start, stop, height), (width, 1)).T
 
-def gradient3d(width, height, start_list, stop_list, is_horizontal_list):
+def gradient3d(width, height, start_list, stop_list):
   result = np.zeros((height, width, len(start_list)), dtype=float)
 
-  for i, (start, stop, is_horizontal) in enumerate(zip(start_list, stop_list, is_horizontal_list)):
-    result[:, :, i] = gradient2d(start, stop, width, height, is_horizontal)
+  for i, (start, stop) in enumerate(zip(start_list, stop_list)):
+    result[:, :, i] = gradient2d(start, stop, width, height)
 
   return result
 
-gradient_color = gradient3d(1280, 720, (47, 46, 48), (32, 31, 34), (False, False, False))
-Image.fromarray(np.uint8(gradient_color)).show()
+gradient_color = gradient3d(1280, 720, (47, 46, 48), (127, 31, 100))
+gradient_result = Image.fromarray(np.uint8(gradient_color))
+gradient_result.show()
+
+print("Gradient done lol")
--- post-processing/post-processing.py
+++ post-processing/post-processing.py
@@ -5,8 +5,8 @@ is accompanied by the background-size effect.
 
 Written by skepfusky
 """
-import argparse
 import os
+
 from PIL import Image, ImageFilter
 
 """
@@ -27,31 +27,35 @@ os.chdir(root_path)
 """
 Some functions to blur crap
 """
-def blur_img(input_img, output_img):
-    img = Image.open(input_img)
-    
-    if "_blur" in input_img:
-      print(f"[NOTICE] {input_img} is already blurred!")
-      return
-    
-    else:
-      if img.format == 'JPEG':
-          print(f"Found JPEG image: {input_img}")
-          gau_blur = img.filter(ImageFilter.GaussianBlur(radius=4))
-          gau_blur.save(output_img.replace('.jpg', '_blur.png'), 'JPEG')
-          return output_img
 
-      else:
-          print(f"Found PNG image: {input_img}")
-          gau_blur = img.filter(ImageFilter.GaussianBlur(radius=4))
-          gau_blur.save(output_img.replace('.png', '_blur.png'), 'PNG')
-          return output_img
+
+def blur_img(input_img, output_img):
+  img = Image.open(input_img)
+  # if "_blur" in img:
+  #   print(f"[NOTICE] {input_img} is already blurred!")
+  #   return output_img
+
+  if img.format == 'JPEG':
+    print(f"Found JPEG image: {input_img}")
+    gau_blur = img.filter(ImageFilter.GaussianBlur(radius=4))
+    gau_blur.save(output_img.replace('.jpg', '_blur.png'), 'JPEG')
+    return output_img
+
+  else:
+    print(f"Found PNG image: {input_img}")
+    gau_blur = img.filter(ImageFilter.GaussianBlur(radius=4))
+    gau_blur.save(output_img.replace('.png', '_blur.png'), 'PNG')
+    return output_img
 
 def blur_folder(input_folder):
-    for img in os.listdir(input_folder):
-        input_img = input_folder + img
-        blur_img(input_img, input_img)
-        
-    return input_folder
+  for img in os.listdir(input_folder):
+    if "_blur" in img:
+      print(f"[NOTICE] {img} is already blurred!")
+
+    else:
+      input_img = input_folder + img
+      blur_img(input_img, input_img)
+
+  return input_folder
 
 blur_folder(f"{os.getcwd()}/public/static/fursonas/comms/")
