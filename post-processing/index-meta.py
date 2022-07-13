b95dac5efe460d7c07ac5f869241089c04a23d50
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-07-13 13:30:11 +0800

    Generate text and crap

--- post-processing/index-meta.py
+++ post-processing/index-meta.py
@@ -1,22 +1,21 @@
 """
-This script randomly generates an image of my fursona, tech stack,
-and stable gradient in the background.
+This script generates an image of my fursona, name, skills,
+and a nice and simple gradient in the background.
 
 Written by skepfusky
 """
-import argparse
+import os
+import textwrap
+import random as rnd
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 
-# Parser stuff
-parser = argparse.ArgumentParser(
-  description='A script that randomly generates an image of my fursona, tech stack, and other cool shit')
-parser.add_argument("-o", "--show-output", action="store_true", help="Show the output image")
-args = parser.parse_args()
+BANNER_WIDTH, BANNER_HEIGHT = (1280, 630)
 
-# Generate a simple, dark af gradient
+# Generate a simple dark purple gradient
 def gradient2d(start, stop, width, height):
   return np.tile(np.linspace(start, stop, height), (width, 1)).T
+
 
 def gradient3d(width, height, start_list, stop_list):
   result = np.zeros((height, width, len(start_list)), dtype=float)
@@ -26,7 +25,43 @@ def gradient3d(width, height, start_list, stop_list):
 
   return result
 
-gradient_color = gradient3d(1280, 720, (47, 46, 48), (127, 31, 100))
-gradient_result = Image.fromarray(np.uint8(gradient_color))
 
-print("Gradient done lol")
+banner_gradient = gradient3d(BANNER_WIDTH, BANNER_HEIGHT, (47, 46, 48), (32, 31, 30))
+banner_result = Image.fromarray(np.uint8(banner_gradient))
+
+# Append text and align it to the center
+header_text = "skepfusky"
+
+# Take the generated gradient and append text on it
+banner_text = ImageDraw.Draw(banner_result)
+font_heading = ImageFont.truetype("fonts/Inter-ExtraBold.ttf", 55)
+font_sub = ImageFont.truetype("fonts/Inter-Medium.ttf", 19)
+
+suffixes = ["broke ass", "a lazy"]
+furry_stuff = ["pattable floof", "fox-husky dude", "soft boi"]
+age = 20
+
+sub_text = f"Hi! I'm a {age}-year-old {rnd.choice(furry_stuff)} hobbyist as a {rnd.choice(suffixes)} programmer," \
+             " filmmaker, indie musician!"
+
+sub_text_break = textwrap.wrap(sub_text, width=69)
+
+banner_text.text((BANNER_WIDTH / 2, BANNER_HEIGHT / 2 + 100), header_text, font=font_heading, fill=(240, 240, 240, 255),
+                 anchor="mt")
+
+y_text = 180
+
+for line in sub_text_break:
+  # TODO: fix this damn deprecationerror
+  width, height = font_sub.getsize(line)
+  banner_text.text((BANNER_WIDTH / 2, BANNER_HEIGHT / 2 + y_text), line, font=font_sub, fill=(240, 240, 240, 255),
+                   anchor="mt")
+  y_text += height
+
+# Show that nice image baby
+# banner_result.show()
+
+# Goes to root directory
+root_path = os.path.dirname(os.getcwd())
+os.chdir(root_path)
+print(root_path)
