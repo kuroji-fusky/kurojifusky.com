2ffbe640fd9a25e5b3793ff60ecf3d0db89f5e2b
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-07-14 07:46:43 +0800

    Fixed DeprecationError and added DOB

--- post-processing/index-meta.py
+++ post-processing/index-meta.py
@@ -5,17 +5,19 @@ and a nice and simple gradient in the background.
 Written by skepfusky
 """
 import os
-import textwrap
-import random as rnd
 import numpy as np
+import textwrap
+from datetime import date
+from random import choice as rnd
 from PIL import Image, ImageDraw, ImageFont
 
 BANNER_WIDTH, BANNER_HEIGHT = (1280, 630)
 
-# Generate a simple dark purple gradient
+"""
+Generate gradient
+"""
 def gradient2d(start, stop, width, height):
   return np.tile(np.linspace(start, stop, height), (width, 1)).T
-
 
 def gradient3d(width, height, start_list, stop_list):
   result = np.zeros((height, width, len(start_list)), dtype=float)
@@ -25,43 +27,49 @@ def gradient3d(width, height, start_list, stop_list):
 
   return result
 
-
 banner_gradient = gradient3d(BANNER_WIDTH, BANNER_HEIGHT, (47, 46, 48), (32, 31, 30))
 banner_result = Image.fromarray(np.uint8(banner_gradient))
 
-# Append text and align it to the center
+"""
+Take the generated gradient, append text and align it to the center
+""" 
 header_text = "skepfusky"
-
-# Take the generated gradient and append text on it
 banner_text = ImageDraw.Draw(banner_result)
 font_heading = ImageFont.truetype("fonts/Inter-ExtraBold.ttf", 55)
 font_sub = ImageFont.truetype("fonts/Inter-Medium.ttf", 19)
 
-suffixes = ["broke ass", "a lazy"]
-furry_stuff = ["pattable floof", "fox-husky dude", "soft boi"]
-age = 20
+suffixes = [" broke ass", " a lazy", ""]
+furry_stuff = ["floofy", "fox-husky", "soft boi"]
 
-sub_text = f"Hi! I'm a {age}-year-old {rnd.choice(furry_stuff)} hobbyist as a {rnd.choice(suffixes)} programmer," \
-             " filmmaker, indie musician!"
+def age(born):
+  today = date.today()
+  return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
+
+age = age(date(2002, 3, 15))
+
+sub_text = f"Hi! I'm a {age}-year-old {rnd(furry_stuff)} hobbyist as a{rnd(suffixes)} programmer," \
+           " filmmaker, indie musician!"
 
 sub_text_break = textwrap.wrap(sub_text, width=69)
 
-banner_text.text((BANNER_WIDTH / 2, BANNER_HEIGHT / 2 + 100), header_text, font=font_heading, fill=(240, 240, 240, 255),
+banner_text.text((BANNER_WIDTH / 2, BANNER_HEIGHT / 2 + 100), header_text, font=font_heading, fill=(232, 232, 232, 255),
                  anchor="mt")
 
 y_text = 180
 
 for line in sub_text_break:
-  # TODO: fix this damn deprecationerror
-  width, height = font_sub.getsize(line)
-  banner_text.text((BANNER_WIDTH / 2, BANNER_HEIGHT / 2 + y_text), line, font=font_sub, fill=(240, 240, 240, 255),
+  height = font_sub.getlength(line[:4])
+  banner_text.text((BANNER_WIDTH / 2, BANNER_HEIGHT / 2 + y_text), line, font=font_sub, fill=(232, 232, 232, 255),
                    anchor="mt")
   y_text += height
 
 # Show that nice image baby
-# banner_result.show()
+banner_result.show()
 
 # Goes to root directory
 root_path = os.path.dirname(os.getcwd())
 os.chdir(root_path)
-print(root_path)
+# print(root_path)
+
+# Saves the image
+# banner_result.save("public/static/meta/banner.png", "PNG")
