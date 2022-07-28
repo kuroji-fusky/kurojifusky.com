f64997069acf84192c363fd6c4a068fe0e1abc5e
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-07-29 02:10:14 +0800

    Not that stupid google font issue again

--- src/components/home/Projects.tsx
+++ src/components/home/Projects.tsx
@@ -5,14 +5,14 @@ export default function Projects() {
   return (
     <div className={styles.wrapper}>
       <h1 className={styles["section-heading"]}>Projects</h1>
-      <div className={styles["responsive-grid"]}>
+      {/* <div className={styles["responsive-grid"]}>
         <ProjectItem
           title="Dumbass"
           description="Eat some ass"
           link="https://openfurs.vercel.app"
           repoLink="skepfusky/pacopanda-drawing-stats"
         />
-      </div>
+      </div> */}
     </div>
   )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,6 +19,10 @@ export default class ShitApp extends Document {
     return (
       <Html>
         <Head />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+        />
         <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" type="image/x-icon" href="/favicon.ico" />
         <body>
