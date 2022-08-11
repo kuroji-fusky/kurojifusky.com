a103d07f507562168d8cb190d6e42d00fff22e35
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-08-11 22:38:52 +0800

    I love pain

--- src/components/home/PreviousWorks.tsx
+++ src/components/home/PreviousWorks.tsx
@@ -24,32 +24,39 @@ export default function PreviousWorks() {
 
 export function WorksItem() {
   const [isOpen, setIsOpen] = useState(true)
-  const [currentHeight, setCurrentHeight] = useState(50)
+  const [currentHeight, setCurrentHeight] = useState(96)
   const heightRef = useRef<HTMLDivElement>(null)
 
   const tall = () => (!isOpen ? setIsOpen(true) : setIsOpen(false))
 
   useEffect(() => {
     !isOpen
-      ? setCurrentHeight(50)
-      : setCurrentHeight(heightRef.current?.offsetWidth ?? 50)
+      ? setCurrentHeight(96)
+      : setCurrentHeight(heightRef.current?.offsetHeight || 96)
 
-    console.log(heightRef.current?.offsetWidth)
-  }, [isOpen])
+    console.log(currentHeight)
+  }, [isOpen, currentHeight])
 
   return (
     <div suppressHydrationWarning>
       {typeof window && (
-        <div
-          className="text-left ml-6 p-3 bg-neutral-900 rounded-md overflow-y-hidden transition-all"
-          ref={heightRef}
-          style={{ height: currentHeight }}
-        >
+        <div className="text-left ml-6 p-3 bg-neutral-900 rounded-md overflow-y-hidden transition-all">
           <strong className="font-inter text-xl block" onClick={tall}>
             Heading
           </strong>
-          <div className="text-sm">
-            asdasdasd
+          <div
+            className="text-sm"
+            ref={heightRef}
+            style={{ height: currentHeight }}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+            voluptates ex quod totam! Distinctio corporis error, omnis voluptas
+            excepturi tempora saepe cum deleniti qui, reiciendis facere iusto
+            alias soluta laudantium molestias atque eaque fuga odio dolores
+            illo? Quibusdam corrupti aspernatur porro saepe odio natus non
+            exlorem ipsum dolor sit amet consectetur adipisicing elit.
+            Recusandae Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Consectetur error quae culpa et dolorum?
           </div>
         </div>
       )}
