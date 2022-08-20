badcae452f478f286ff39588356f61103c560b59
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-08-20 21:07:39 +0800

    Add GTM

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -33,8 +33,8 @@ export default class ShitApp extends Document {
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA}`}
           strategy="worker"
         ></Script>
-        <script
-          type="text/partytown"
+        <Script
+          strategy="worker"
           dangerouslySetInnerHTML={{
             __html: `
             window.dataLayer = window.dataLayer || [];
@@ -46,9 +46,26 @@ export default class ShitApp extends Document {
             `
           }}
         />
+        <Script
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${process.env.GTM}');
+            `
+          }}
+        />
         <body>
           <Main />
           <NextScript />
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.GTM}"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+            }}
+          ></noscript>
         </body>
       </Html>
     )
