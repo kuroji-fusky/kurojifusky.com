e50e36f411d1bcca1bd4067394746fc0a15cb423
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-10-27 12:34:53 +0800

    Remove non-existant shit

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,19 +1,12 @@
-import { Container } from "@/components/Base";
-import { PortfolioItem } from "@/components/Items";
+import { Container } from "@/components/Base"
 
 export default function PortfolioPage() {
-  const title = "Portfolio";
-  const description = "lmao";
+  const title = "Portfolio"
+  const description = "lmao"
 
   return (
     <Container wrap title={title} description={description}>
       <h1>Portfolio</h1>
-      <div className="grid grid-cols-3 gap-5">
-        <PortfolioItem />
-        <PortfolioItem />
-        <PortfolioItem />
-        <PortfolioItem />
-      </div>
     </Container>
-  );
+  )
 }
