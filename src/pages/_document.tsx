37c6940f0cb1eb3a8f18dbdf616a3f4ea1f0749d
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-11-16 07:51:29 +0800

    Add hotjar

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -36,6 +36,18 @@ export default class _ extends Document {
             });
         `}
           </Script>
+          <Script id="hotjar" strategy="lazyOnload">
+            {`
+    			(function(h,o,t,j,a,r){
+        	h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        	h._hjSettings={hjid:3249416,hjsv:6};
+        	a=o.getElementsByTagName('head')[0];
+        	r=o.createElement('script');r.async=1;
+        	r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        	a.appendChild(r);
+    			})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+					`}
+          </Script>
           <Script
             strategy="lazyOnload"
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA}`}
