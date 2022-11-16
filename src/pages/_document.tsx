e4af4e649a2c45b7cb8f8fc30b078932f13c3d23
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-11-16 08:02:04 +0800

    Place hotjar on head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -25,18 +25,8 @@ export default class _ extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&family=JetBrains+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
           />
-          <Script id="ga" strategy="lazyOnload">
-            {`
-            window.dataLayer = window.dataLayer || [];
-            window.gtag = function gtag(){window.dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', '${process.env.GA}', { 
-                page_path: window.location.pathname,
-            });
-        `}
-          </Script>
-          <Script id="hotjar" strategy="lazyOnload">
+          {/* Trackers and shit */}
+          <script>
             {`
     			(function(h,o,t,j,a,r){
         	h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
@@ -47,6 +37,17 @@ export default class _ extends Document {
         	a.appendChild(r);
     			})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
 					`}
+          </script>
+          <Script id="ga" strategy="lazyOnload">
+            {`
+            window.dataLayer = window.dataLayer || [];
+            window.gtag = function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.GA}', { 
+                page_path: window.location.pathname,
+            });
+        `}
           </Script>
           <Script
             strategy="lazyOnload"
