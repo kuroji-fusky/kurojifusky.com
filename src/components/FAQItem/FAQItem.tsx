4d9bec58c78c8bbdf0e1cbb3d0e98c6cfe806b39
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-11-26 19:25:21 +0800

    FAQ item partial done, but I'm hungry

--- src/components/FAQItem/FAQItem.tsx
+++ src/components/FAQItem/FAQItem.tsx
@@ -4,6 +4,7 @@ import ReactMarkdown from "react-markdown"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons"
 import Link from "next/link"
+import styles from "./FAQItem.module.scss"
 
 interface FAQProps {
   question: string
@@ -11,53 +12,47 @@ interface FAQProps {
 }
 
 export default function FAQItem(props: FAQProps) {
-  const lul = useRef<HTMLElement>(null)
+  const ref = useRef<HTMLElement>(null)
   const [toggle, setToggle] = useState(false)
 
   const toggleAnimation = {
-    height: !toggle ? 0 : lul.current?.scrollHeight,
+    height: !toggle ? 0 : ref.current?.scrollHeight,
     opacity: !toggle ? 0 : 1,
     y: !toggle ? 0 : -6
   }
 
   return (
     <div
-      id="faq-item"
-      className={`w-full rounded-md transition-[border] duration-300 border-2 ${
+      id={styles["wrapper"]}
+      className={
         !toggle ? "border-sona-borahae-600" : "border-sona-skycyan-400"
-      }`}
+      }
     >
       <button
-        className="p-5 xl:p-6 w-full flex justify-between items-center"
+        className={styles["question-toggle"]}
         onClick={() => setToggle(!toggle)}
       >
-        <h2 className="text-2xl xl:text-[1.75rem] text-left mr-4">
-          {props.question}
-        </h2>
+        <h2 className={styles["question-heading"]}>{props.question}</h2>
         <FontAwesomeIcon
           icon={!toggle ? faPlus : faMinus}
           size="lg"
           rotation={!toggle ? undefined : 180}
-					className="transition-[transform] duration-300"
+          className="transition-[transform] duration-300"
         />
       </button>
       <motion.article
-        ref={lul}
+        ref={ref}
         className={`overflow-hidden${!toggle ? " pointer-events-none" : ""}`}
         initial={{ height: 0 }}
         animate={toggleAnimation}
         transition={{ duration: 0.2 }}
       >
         <ReactMarkdown
-          className="px-6 pt-0 pb-3 text-base xl:text-lg flex flex-col gap-3"
+          className={styles["md-wrapper"]}
           components={{
             a: ({ node, ...props }) => (
               <Link href={props.href ?? ""} passHref>
-                <a
-                  {...props}
-                  className="anchor-underline text-sona-borahaealt-200 inline-block relative w-fit hover:text-sona-borahaealt-300
-								before:content-[''] before:bg-sona-borahaealt-300 before:absolute before:bottom-0 before:left-0 before:w-full before:h-[1px]"
-                ></a>
+                <a {...props} className={styles["md-links"]}></a>
               </Link>
             )
           }}
