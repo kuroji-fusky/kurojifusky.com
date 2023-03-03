c8f59714df6ad4e70492b489a85216368d77c7f1
Kerby Keith Aquino <skepfoosky15@gmail.com>
2023-03-03 07:21:09 +0000

    this is pain

--- components/TheBaseFooter.vue
+++ components/TheBaseFooter.vue
@@ -1,62 +1,54 @@
 <script setup lang="ts">
-import { footerLinks } from "./Socials"
 const currentYear = new Date().getFullYear()
 const copyright = `© 2013-${currentYear} Kerby Keith Aquino`
+
+const footerLinks = [
+	{
+		link: "https://ko-fi.com/kuroji_fusky",
+		text: "Support my broke ass",
+		external: true,
+	},
+	{
+		link: "https://github.com/kuroji-fusky/kurofusky.xyz",
+		text: "View this website's source code",
+		external: true,
+	},
+]
 </script>
 
 <template>
-	<footer>
-		<ul class="socials">
-			<li v-for="(item, index) in footerLinks" :key="index">
-				<FooterSocial :name="item.name" :link="item.link" :icon="item.icon" />
+	<footer class="flex gap-3.5 px-10 py-12 font-inter">
+		<ul class="w-full grid grid-flow-col gap-6">
+			<li
+				class="border-r border-r-sona-borahaealt-500 text-opacity-75 text-sona-borahaealt-50"
+			>
+				<p>
+					{{ copyright }}<br />
+					All rights reserved.
+				</p>
+			</li>
+			<li>
+				<p>
+					Reach me out directly:<br>
+					<NuxtLink
+						class="text-sona-borahaealt-100 hover:text-opacity-100 text-opacity-75 transition-colors"
+						to="mailto:hello@kurofusky.xyz"
+						>hello@kurofusky.xyz</NuxtLink
+					>
+				</p>
+			</li>
+			<li v-for="item in footerLinks">
+				<NuxtLink
+					class="text-sona-borahaealt-100 hover:text-opacity-100 text-opacity-75 transition-colors"
+					:to="item.link"
+					:target="`${!item.external ? '' : '_blank'}`"
+					:rel="`${!item.external ? undefined : 'noreferrer'}`"
+				>
+					{{ item.text }}
+				</NuxtLink>
 			</li>
 		</ul>
-		<div class="links">
-			<NuxtLink
-				class="link-underline"
-				to="https://ko-fi.com/kuroji_fusky"
-				target="_blank"
-				external
-				>Support my broke ass
-			</NuxtLink>
-			<NuxtLink
-				class="link-underline"
-				to="https://github.com/kuroji-fusky/kurofusky.xyz"
-				target="_blank"
-				external
-			>
-				Website source code
-			</NuxtLink>
-		</div>
-		<span id="copyright"> {{ copyright }}</span>
 	</footer>
 </template>
 
-<style lang="scss" scoped>
-footer {
-	@apply grid grid-cols-2 grid-rows-2 gap-3.5 px-12 py-8;
-
-	grid-template-areas:
-		"socials links"
-		"socials copyright";
-}
-
-.socials {
-	@apply flex text-xl gap-8 items-center;
-	grid-area: socials;
-}
-
-#copyright {
-	grid-area: copyright;
-	@apply gap-1 #{!important};
-}
-
-.links {
-	grid-area: links;
-	@apply font-jetbrains-mono;
-}
-
-:is(.links, #copyright) {
-	@apply text-sm flex justify-end gap-3;
-}
-</style>
+<style lang="scss"></style>
