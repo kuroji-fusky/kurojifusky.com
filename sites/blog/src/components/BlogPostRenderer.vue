b5d6fdac3b3b2f255ce3504674de1d250ba215e5
Kerby Keith Aquino <skepfoosky15@gmail.com>
2023-09-19 23:58:00 +0800

    got sum basic af blog layout

--- sites/blog/src/components/BlogPostRenderer.vue
+++ sites/blog/src/components/BlogPostRenderer.vue
@@ -1,5 +1,52 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineProps<
+  Partial<{
+    title: string
+    description: string
+    date: string | unknown
+    img: string
+    category: string[]
+    big: boolean
+  }>
+>()
+</script>
 
 <template>
-  <div></div>
+  <article :class="['flex gap-7', !big ? 'flex-row' : 'flex-col']">
+    <a
+      href="#"
+      :class="[
+        'flex relative before:absolute before:block before:inset-0',
+        !big && 'mb-auto flex-shrink-0 w-2/6'
+      ]"
+      draggable="false"
+    >
+      <img
+        :class="[
+          'object-cover w-full h-full rounded-lg ',
+          !big ? 'aspect-square' : 'aspect-[12/6]'
+        ]"
+        :src="
+          img ||
+          'https://images.ctfassets.net/1wy7cxqsia50/mAZvCqagMSGHRNBRCo6fG/50ad301404a132586b73b165f648a658/Untitled101_20230316135154.png'
+        "
+        alt=""
+        draggable="false"
+      />
+    </a>
+    <div class="flex flex-col">
+      <div class="flex flex-wrap gap-4">
+        <span>Category</span>
+        <span>Category</span>
+        <span>Category</span>
+        <span>Category</span>
+      </div>
+      <h2 :class="['my-4', !big ? 'text-3xl' : 'text-5xl']">
+        {{ title }}
+      </h2>
+      <p class="text-lg leading-7">
+        {{ description }}
+      </p>
+    </div>
+  </article>
 </template>
--- sites/blog/src/components/BlogShelf.vue
+++ sites/blog/src/components/BlogShelf.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import BlogPostRenderer from "./BlogPostRenderer.vue"
+
 const lmao = [
   { name: "All", filter: "" },
   { name: "Updates", filter: "updates" },
@@ -11,7 +13,7 @@ const lmao = [
 
 <template>
   <section class="pb-4 pt-6">
-    <nav class="flex justify-center gap-x-2.5 select-none">
+    <!-- <nav class="flex justify-center gap-x-2.5 select-none">
       <button
         v-for="({ name, filter }, i) in lmao"
         :key="i"
@@ -19,10 +21,33 @@ const lmao = [
       >
         <span class="whitespace-nowrap">{{ name }}</span>
       </button>
-    </nav>
+    </nav> -->
   </section>
   <section>
-    <div class="w-full">picked posts</div>
+    <div class="w-full flex gap-12">
+      <BlogPostRenderer
+        class="flex-shrink-0 w-[60%]"
+        big
+        img="https://images.ctfassets.net/1wy7cxqsia50/5wIqmoYZ0I1f21GSf1UKZH/a71fa5c9dc378a8ebb36a5288b2365b0/Banner_hero_1_.png"
+        title="A new chapter for MyFursona"
+        description="A soon-to-be social art platform to cater artists and users for managing their characters and keep track of commissions and adoptables"
+      />
+      <div class="flex flex-col gap-y-9">
+        <BlogPostRenderer
+          title="Beware of fake and cheap fursuits!"
+          description="There are ripoff fursuits from Chinese e-commerce websites, why you shouldn't buy these suits, and look for their original suiters that are commonly ripped from scammers!"
+        />
+        <BlogPostRenderer
+          title="Programming Language's Supersets"
+          img="https://images.ctfassets.net/1wy7cxqsia50/73ZC5QBXyJxv3h4dXQYYau/0d6a1f06b5187967483a71c5f40735e6/Frame_70.png"
+          description='In the world of software development, we often encounter terms like "superset" and "subset" when discussing the relationships between programming languages.'
+        />
+        <BlogPostRenderer
+          title='Origins of my "Kuroji Fusky" fursona'
+          description="How I made my fox-husky character lmfao"
+        />
+      </div>
+    </div>
     <aside class="w-2/6 flex-shrink-0">
       <h2>Latest posts</h2>
     </aside>
