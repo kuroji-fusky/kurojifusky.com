15a940549de396d486e465ba7c762554320214a8
Kerby Keith Aquino <skepfoosky15@gmail.com>
2023-10-10 02:01:30 +0800

    add mouse positioning thing

--- portfolio/src/components/Landing/Hero.tsx
+++ portfolio/src/components/Landing/Hero.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useEffect, useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import HeroImg from "./HeroImg"
 import clsx from "clsx"
 
@@ -35,6 +35,11 @@ export default function Hero() {
   ]
 
   const [artworkIndex, setArtworkIndex] = useState(4)
+  const [artworkPos, setArtworkPos] = useState({
+    x: 0,
+    y: 0
+  })
+
   const heroRootRef = useRef<HTMLDivElement>(null)
   const testing = useRef<HTMLDivElement>(null)
 
@@ -55,40 +60,82 @@ export default function Hero() {
 
     const io = new IntersectionObserver(([entry]) => {
       if (entry.isIntersecting) {
-        window.addEventListener("mousemove", handleMousePosIndex)
+        heroSection!.addEventListener("mousemove", handleMousePosIndex)
         return
       }
-      window.removeEventListener("mousemove", handleMousePosIndex)
+      heroSection!.removeEventListener("mousemove", handleMousePosIndex)
       return
     })
 
     io.observe(heroSection!)
 
     return () => {
-      window.removeEventListener("mousemove", handleMousePosIndex)
+      heroSection!.removeEventListener("mousemove", handleMousePosIndex)
     }
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
+  useEffect(() => {
+    const heroSection = heroRootRef.current
+    const lol = testing.current
+
+    const handleRelativePosition = ({ x: mouseX, y: mouseY }: MouseEvent) => {
+      const windowWidth = window.innerWidth
+      const windowHeight = window.innerHeight
+
+      const relX = -1 + (mouseX / windowWidth) * 2
+      const relY = -1 + (mouseY / windowHeight) * 2
+
+      setArtworkPos({
+        x: relX * 320,
+        y: relY * 150
+      })
+    }
+
+    const io = new IntersectionObserver(([entry]) => {
+      if (entry.isIntersecting) {
+        heroSection!.addEventListener("mousemove", handleRelativePosition)
+        return
+      }
+      heroSection!.removeEventListener("mousemove", handleRelativePosition)
+      return
+    })
+
+    io.observe(heroSection!)
+
+    return () => {
+      heroSection!.removeEventListener("mousemove", handleRelativePosition)
+    }
+  }, [])
 
   return (
     <div className="h-[calc(100dvh-4.75rem)]" ref={heroRootRef}>
       <div
         className="grid absolute inset-0 place-items-center"
-        style={{ "--img-size": "18.5" } as React.CSSProperties}
+        style={
+          {
+            "--img-size": "18",
+            "--computed-size":
+              "calc(calc(calc(var(--img-size) / 2) * -1) * 1rem)"
+          } as React.CSSProperties
+        }
       >
         <div
-          className="flex md:block relative
-          left-[calc(calc(calc(var(--img-size)/2)*-1)*1rem)]
-          top-[calc(calc(calc(var(--img-size)/2)*-1)*1rem)]
-          "
+          className="flex md:block relative left-[var(--computed-size)] top-[var(--computed-size)]"
           ref={testing}
+          style={{
+            transform: `translate3d(${artworkPos.x}px, ${artworkPos.y}px, 0px)`
+          }}
         >
           {artworks.map((item, index) => (
-            <div
+            <span
               key={index}
-              className={clsx(index === artworkIndex ? "visible" : "invisible")}
+              className={clsx(
+                "contents",
+                index === artworkIndex ? "visible" : "invisible"
+              )}
             >
               <HeroImg img={item.url} artist={item.artist} />
-            </div>
+            </span>
           ))}
         </div>
       </div>
--- portfolio/tailwind.config.ts
+++ portfolio/tailwind.config.ts
@@ -1,7 +1,7 @@
 import kuroConfig from "@kuro/ui/tailwind.config"
 import type { Config } from "tailwindcss"
 
-const config: Config = {
+export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -16,6 +16,4 @@ const config: Config = {
       }
     }
   }
-}
-
-export default config
+} as Config
