05276fb51b78029eef74bf2f3024a70959e6b3ab
Kerby Keith Aquino <skepfoosky15@gmail.com>
2023-10-10 22:19:18 +0800

    blog astro stuff, update deps

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -1,19 +1,15 @@
-import { defineConfig } from "astro/config"
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+import prefetch from "@astrojs/prefetch";
+import sitemap from "@astrojs/sitemap";
 
-import tailwind from "@astrojs/tailwind"
-import image from "@astrojs/image"
-import prefetch from "@astrojs/prefetch"
-import sitemap from "@astrojs/sitemap"
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    image(),
-    prefetch(),
-    sitemap({
-      changefreq: "hourly"
-    })
-  ],
+  integrations: [tailwind(), image(), prefetch(), sitemap({
+    changefreq: "hourly"
+  }), react()],
   site: "https://blog.kurojifusky.com/"
-})
+});
