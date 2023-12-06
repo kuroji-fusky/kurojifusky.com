cb5a542dcb69ba158602f143e5769cb70a97f74f
Kerby Keith Aquino <skepfoosky15@gmail.com>
2023-12-06 21:15:57 +0800

    NO MORE WAFFLES GIR
    add husky
    
    add lint-staged
    
    fuck

--- portfolio/components/Navbar.vue
+++ portfolio/components/Navbar.vue
@@ -11,27 +11,27 @@ const topNavMenus = [
 
 <template>
   <div class="sticky inset-0 bottom-0 z-10">
+    <header class="px-8 py-5 flex justify-between z-[5]">
+      <NuxtLink to="/" aria-label="Home" title="Home">
+        <KuroLogo :width="150" />
+      </NuxtLink>
+      <div class="flex items-center text-base gap-x-8">
+        <nav class="flex items-center gap-x-4 select-none">
+          <NuxtLink
+            v-for="(link, index) in topNavMenus"
+            :key="index"
+            :target="link.path.startsWith('https:') ? '_blank' : null"
+            :to="link.path"
+          >
+            <span>{{ link.name }}</span>
+          </NuxtLink>
+        </nav>
+        <button>
+          <LucideMenu />
+        </button>
+      </div>
+    </header>
     <div class="relative">
-      <header class="px-8 py-5 flex justify-between z-[5]">
-        <NuxtLink to="/" aria-label="Home" title="Home">
-          <KuroLogo :width="150" />
-        </NuxtLink>
-        <div class="flex items-center text-base gap-x-8">
-          <nav class="flex items-center gap-x-4 select-none">
-            <NuxtLink
-              v-for="(link, index) in topNavMenus"
-              :key="index"
-              :target="link.path.startsWith('https:') ? '_blank' : null"
-              :to="link.path"
-            >
-              <span>{{ link.name }}</span>
-            </NuxtLink>
-          </nav>
-          <button>
-            <LucideMenu />
-          </button>
-        </div>
-      </header>
       <div
         class="absolute inset-0 bg-kuro-lavender-900 bg-opacity-25 backdrop-blur-sm"
       />
--- shared/_urls.ts
+++ shared/_urls.ts
@@ -1,5 +1,5 @@
 export default {
   main: "https://kurojifusky.com",
   blog: "https://blog.kurojifusky.com",
-  umami: "https://eu.umami.is/script.js"
+  umami: "https://eu.umami.is/script.js",
 }
