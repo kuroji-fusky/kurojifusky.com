828669c7723d3228f2751b96caa53f374c43a636
Kerby Keith Aquino <skepfoosky15@gmail.com>
2023-12-08 22:10:31 +0800

    update blog ui

---
+++ blog/src/components/Footer.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { socials } from "@kuro/shared"
+</script>
+
+<template>
+  <footer class="px-8 mx-auto max-w-screen-2xl">
+    <div class="flex justify-between">
+      <div class="flex items-center gap-x-2">
+        <a
+          v-for="(social, index) in socials"
+          :key="index"
+          :title="social.name"
+          :href="social.link"
+          rel="nofollower me"
+          target="_blank"
+        >
+          <component :is="social.icon" width="1.6rem" height="1.25rem" />
+        </a>
+      </div>
+      <span>Version 1.1</span>
+    </div>
+  </footer>
+</template>
