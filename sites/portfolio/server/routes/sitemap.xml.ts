9151d6d03fd6bfd906e164f4cb997b4435ee0fb2
Kerby Keith Aquino <skepfoosky15@gmail.com>
2024-02-03 18:53:36 +0800

    OOOH IT'S JUNGKOOK

---
+++ sites/portfolio/server/routes/sitemap.xml.ts
@@ -0,0 +1,21 @@
+import { baseUrls } from "@kuro/shared"
+import { serverQueryContent } from "#content/server"
+import { SitemapStream, streamToPromise } from "sitemap"
+
+export default defineEventHandler(async (event) => {
+  // Fetch all documents
+  const docs = await serverQueryContent(event).find()
+  const sitemap = new SitemapStream({
+    hostname: baseUrls.main
+  })
+
+  for (const doc of docs) {
+    sitemap.write({
+      url: doc._path,
+      changefreq: "weekly"
+    })
+  }
+  sitemap.end()
+
+  return streamToPromise(sitemap)
+})
