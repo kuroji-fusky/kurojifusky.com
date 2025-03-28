bc8fec806b9a6e1e79266517f6667e3a06d6faf3
Kerby Keith Aquino <hello@kurojifusky.com>
2025-03-29 03:59:57 +0800

    remove svelte; keeping things supa pure

--- src/stores/index.ts
+++
@@ -1,4 +0,0 @@
-import { writable } from "svelte/store"
-
-export const navTitle = writable<string>("")
-export const isTitleShown = writable<boolean>(false)
--- src/stores/togglerStore.ts
+++
@@ -1,9 +0,0 @@
-import { writable } from "svelte/store"
-
-export const togglerStore = (state: boolean) => {
-  const { subscribe, update } = writable(state)
-
-  const toggleState = () => update((prevState) => (prevState = !prevState))
-
-  return { subscribe, toggleState }
-}
