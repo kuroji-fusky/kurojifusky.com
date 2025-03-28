d9124f2165eacff3cfc3007c99ff7f816a2e3ed3
Kerby Keith Aquino <hello@kurojifusky.com>
2025-03-29 03:22:24 +0800

    goodbye contentful, it's been nice serving you

--- src/lib/contentful/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./types"
-export * from "./fetchBlogAuthor"
-export * from "./fetchBlogPosts"
-export * from "./fetchBlogSeries"
-export * from "./fetchVaultContent"
-
-export * from "./parsers/parseContent"
