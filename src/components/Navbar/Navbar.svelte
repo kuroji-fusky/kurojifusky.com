aa05d4521161d0091bfeb6ccb3717e39999d26e7
Kuro <hello@kurojifusky.com>
2025-05-30 06:10:30 +0800

    adding gsap cause fuk you that's why

--- src/components/Navbar/Navbar.svelte
+++ src/components/Navbar/Navbar.svelte
@@ -3,8 +3,10 @@
   import SearchIcon from "~icons/lucide/search?raw";
   import MenuIcon from "~icons/lucide/menu?raw";
   import ChevronDownIcon from "~icons/lucide/chevron-down?raw";
-  import { onMount } from "svelte";
   import { navLinks } from "./NavbarItems";
+  import { onMount } from "svelte";
+
+  import { gsap } from "gsap";
 
   interface Props {
     activeRoute: string;
@@ -14,7 +16,7 @@
 
   let menuState = $state(false);
 
-  const toggleMenu = () => (menuState = !menuState);
+  onMount(() => {});
 </script>
 
 <div class="z-20 fixed inset-x-0 top-0 bg-kuro-dark2/80 backdrop-blur-md">
@@ -47,7 +49,6 @@
       {/each}
       <span class="hidden lg:inline-block h-5 border-l opacity-40 mx-2"></span>
       <button
-        onclick={toggleMenu}
         class="cursor-pointer hidden lg:block px-2 py-1 flex-shrink-0 [&_svg]:!h-[1.5rem] relative before:duration-[250ms] before:pointer-events-none before:absolute before:-z-10 before:rounded-sm before:size-full before:inset-0 before:transition-all hover:before:bg-kuro-lavender-200/20 before:scale-[0.96] hover:before:scale-100"
       >
         {@html ChevronDownIcon}
@@ -59,7 +60,7 @@
       <button>
         {@html SearchIcon}
       </button>
-      <button class="lg:hidden block" onclick={toggleMenu}>
+      <button class="lg:hidden block">
         {@html MenuIcon}
       </button>
     </div>
