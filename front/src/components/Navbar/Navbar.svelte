6a8a373126edf1bfa68fe3b803a04cbefa4dfd4b
Kuro <hello@kurojifusky.com>
2025-06-02 21:16:11 +0800

    finished that big nav menu thing
    - remove unsolved `components` import from MDX files
    - updated 404 page

--- front/src/components/Navbar/Navbar.svelte
+++ front/src/components/Navbar/Navbar.svelte
@@ -2,68 +2,126 @@
   import LogoBrand from "~icons/kuro/logo-brand?raw";
   import SearchIcon from "~icons/lucide/search?raw";
   import MenuIcon from "~icons/lucide/menu?raw";
+  import CloseIcon from "~icons/lucide/x?raw";
   import ChevronDownIcon from "~icons/lucide/chevron-down?raw";
-  import { navLinks } from "./NavbarItems";
   import { onMount } from "svelte";
 
   import { gsap } from "gsap";
 
   interface Props {
     activeRoute: string;
+    inlineNavItems: object;
   }
 
-  const { activeRoute }: Props = $props();
+  const { activeRoute, inlineNavItems }: Props = $props();
+
+  let isOpen = $state(false);
 
   let navRootScope: HTMLDivElement;
 
-  let navDesktop: HTMLDivElement;
-  let navButtonDesktop: HTMLButtonElement;
+  let navContainer: HTMLDivElement;
+  let navButton: HTMLButtonElement;
+  let navClose: HTMLButtonElement;
 
-  let navLinksCurtainEl: HTMLElement;
+  let navLinksCurtain: HTMLElement;
 
   onMount(() => {
     const gsapCtx = gsap.context(() => {
-      // will create a timeline for this for later, in the meantime, this is MVP worthy
-      const miniNav = gsap
-        .to(navDesktop, {
-          y: 15,
-          opacity: 0.25,
-          filter: "blur(2px)",
-          ease: "power3",
-        })
-        .reversed(true);
-
-      const navLinksCurtain = gsap
-        .fromTo(
-          navLinksCurtainEl,
-          { height: "0%", pointerEvents: "none", ease: "power3" },
-          { height: "100%", pointerEvents: "auto", ease: "power3" },
-        )
-        .reversed(true);
-
-      navButtonDesktop.addEventListener("click", () => {
-        const _h = document.documentElement;
-        const isTheBitchReversed = miniNav.reversed();
-
-        if (isTheBitchReversed) {
-          miniNav.play();
-          navLinksCurtain.play();
-          _h.classList.add("overflow-y-hidden");
-        } else {
-          miniNav.reverse();
-          navLinksCurtain.reverse();
-          _h.classList.remove("overflow-y-hidden");
-        }
+      const navAnimTree = gsap.timeline().reversed(true);
+      navAnimTree.to(navContainer, {
+        y: 6,
+        opacity: 0,
+        filter: "blur(2px)",
+        ease: "power2.inOut",
+        pointerEvents: "none",
+        duration: 0.33,
       });
+
+      navAnimTree.fromTo(
+        navLinksCurtain,
+        {
+          height: "0%",
+          pointerEvents: "none",
+          display: "none",
+        },
+        {
+          height: "100%",
+          pointerEvents: "auto",
+          ease: "expo.inOut",
+          duration: 0.48,
+          display: "block",
+        },
+        "-=0.35",
+      );
+
+      // descendant to navLinksCurtainEl
+      navAnimTree.fromTo(
+        navLinksCurtain.querySelector("& > div"),
+        {
+          y: -33,
+          pointerEvents: "none",
+          filter: "blur(2px)",
+          opacity: 0.15,
+        },
+        {
+          y: 0,
+          pointerEvents: "auto",
+          filter: "blur(0px)",
+          opacity: 1,
+          ease: "expo.inOut",
+          duration: 0.75,
+        },
+        "-=0.65",
+      );
+
+      // Event listeners; desktop navigation
+      const _h = document.documentElement;
+
+      const handleCloseMenu = () => {
+        if (!isOpen) return;
+        navAnimTree.reverse();
+        _h.classList.remove("overflow-y-hidden");
+
+        isOpen = false;
+        return;
+      };
+
+      const handleEscKey = ({ key }: KeyboardEvent) => {
+        if (key === "Escape" && isOpen) {
+          handleCloseMenu();
+
+          // Remove keyboard listener anymore if the bitch is closed
+          window.removeEventListener("keydown", handleEscKey);
+          return;
+        }
+      };
+
+      // Opening menu
+      navButton.addEventListener("click", () => {
+        if (isOpen && !navAnimTree.reversed()) return;
+
+        // Listen for ESC key presses
+        window.addEventListener("keydown", handleEscKey);
+
+        navAnimTree.play();
+        _h.classList.add("overflow-y-hidden");
+        isOpen = true;
+      });
+
+      // Closing menu
+      navClose.addEventListener("click", handleCloseMenu);
     }, navRootScope);
 
-    return () => {
-      gsapCtx.revert();
-    };
+    return () => gsapCtx.revert();
   });
 </script>
 
-<!-- I'm too lazy to wrap this into separate components, this my code fuk u -->
+<!--
+I'm too lazy to wrap this into separate components
+I'm aware the code or this is a mess, leave me alone
+
+this my code not urs fuk u
+-->
 <div bind:this={navRootScope} class="contents">
   <div class="z-50 fixed inset-x-0 top-0 bg-kuro-dark2/80 backdrop-blur-md">
     <nav
@@ -78,14 +136,14 @@
       </a>
 
       <div
-        bind:this={navDesktop}
+        bind:this={navContainer}
         class="flex lg:relative lg:bg-transparent lg:flex-row lg:top-0 lg:items-center absolute flex-col top-14 items-start w-max justify-end my-auto gap-x-1 font-kuro-mono text-sm"
       >
-        {#each navLinks as { href, text }}
+        {#each inlineNavItems as { href, text }}
           <a
             {href}
             class={[
-              "relative before:absolute before:inset-0 before:transition-all before:duration-[250ms] before:pointer-events-none before:rounded-sm hover:before:bg-kuro-lavender-100/20 before:scale-[0.96] hover:before:scale-100",
+              "squishy-button",
               "px-3 py-1.5",
               activeRoute.startsWith(href) ? "text-kuro-lavender-200" : null,
             ]}
@@ -98,8 +156,8 @@
           <span class="hidden lg:inline-block h-5 border-l opacity-40 mx-2"
           ></span>
           <button
-            bind:this={navButtonDesktop}
-            class="cursor-pointer hidden lg:block px-2 py-1 flex-shrink-0 [&_svg]:!h-[1.5rem] relative before:duration-[250ms] before:pointer-events-none before:absolute before:-z-10 before:rounded-sm before:size-full before:inset-0 before:transition-all hover:before:bg-kuro-lavender-200/20 before:scale-[0.96] hover:before:scale-100"
+            bind:this={navButton}
+            class="cursor-pointer hidden lg:block px-2 py-1 flex-shrink-0 [&_svg]:!h-[1.5rem] squishy-button"
           >
             {@html ChevronDownIcon}
           </button>
@@ -118,16 +176,32 @@
     </nav>
   </div>
 
-  <!-- All nav things -->
+  <!-- Desktop navigation lol -->
   <aside
-    bind:this={navLinksCurtainEl}
-    class="z-40 fixed inset-x-0 top-0 bg-kuro-dark1 overflow-hidden"
+    bind:this={navLinksCurtain}
+    class="select-none z-40 fixed inset-x-0 top-0 bg-kuro-dark1 overflow-hidden"
     style="height: 0%; pointer-events:none"
   >
-    <div class="mx-auto max-w-screen-lg px-6 pt-24 *:px-6 space-y-6">
-      <section>Explore the cringe</section>
+    <div class="mx-auto max-w-screen-lg px-6 pt-20 *:px-6 space-y-6">
+      <div
+        class="flex justify-between items-center pb-4 border-b border-b-kuro-lavender-200/30"
+      >
+        <span class="font-bold text-2xl">Explore</span>
+
+        <div class="inline-flex items-center gap-x-1">
+          <kbd class="bg-kuro-lavender-100/20 px-1 py-1 rounded-md text-xs"
+            >&lbrack;ESC&rbrack;</kbd
+          >
+          <button
+            bind:this={navClose}
+            class="p-2 rounded-md cursor-pointer squishy-button"
+          >
+            {@html CloseIcon}
+          </button>
+        </div>
+      </div>
       <section class="grid grid-flow-col grid-cols-3 grid-rows-2 gap-12">
-        {#each navLinks as { href: hrefHeading, text: textHeading, subitems, rowSpan }}
+        {#each inlineNavItems as { href: hrefHeading, text: textHeading, subitems, rowSpan }}
           <div
             role="listbox"
             class="space-y-2"
@@ -136,7 +210,9 @@
               : undefined}
           >
             <div class="mb-4 opacity-65">
-              <a href={hrefHeading} class="font-kuro-mono">{textHeading}</a>
+              <a href={hrefHeading} class="font-kuro-mono uppercase"
+                >{textHeading}</a
+              >
             </div>
             {#each subitems as { href, text }}
               <a {href} class="block text-lg">{text}</a>
@@ -148,3 +224,11 @@
   </aside>
 </div>
 <div class="h-12"></div>
+
+<style lang="postcss">
+  @reference "../../styles/global.css";
+
+  .squishy-button {
+    @apply relative before:duration-[250ms] before:pointer-events-none before:absolute before:-z-10 before:rounded-sm before:size-full before:inset-0 before:transition-all hover:before:bg-kuro-lavender-200/20 before:scale-[0.96] hover:before:scale-100;
+  }
+</style>
