394cf8800386f629dc87752c2c393a29621f7099
Kuro <hello@kurojifusky.com>
2025-06-02 21:16:46 +0800

    updated client heavy components to svelte

--- front/src/components/landing/Hero.svelte
+++ front/src/components/landing/Hero.svelte
@@ -1,6 +1,6 @@
----
-import ImageWrapper from "$components/ImageWrapper.astro";
----
+<script lang="ts">
+  import ImageWrapper from "$components/ImageWrapper.svelte";
+</script>
 
 <div class="h-[calc(100dvh-3.25rem)]">
   <div class="grid place-items-center h-full">
