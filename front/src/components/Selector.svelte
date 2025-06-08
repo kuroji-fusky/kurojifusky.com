adbc0cfb1504c01a44dc55eae9992c4223f9b4b2
Kuro <hello@kurojifusky.com>
2025-06-08 21:15:14 +0800

    more artwork example dummies
    - add embed and selector components

---
+++ front/src/components/Selector.svelte
@@ -0,0 +1,40 @@
+<script
+  lang="ts"
+  generics="T extends readonly { key: string, href: string, label: string }[]"
+>
+  import { cn } from "$lib/utils";
+
+  type DeepMutable<T> =
+    T extends ReadonlyArray<infer R>
+      ? DeepMutable<R>[]
+      : T extends object
+        ? { -readonly [P in keyof T]: DeepMutable<T[P]> }
+        : T;
+
+  interface Props {
+    options: DeepMutable<T>;
+    defaultVal: T[number]["key"];
+  }
+
+  const { options, defaultVal }: Props = $props();
+</script>
+
+<div>
+  <!-- <div>{options.find((x) => x.key === defaultVal)!.label}</div> -->
+  <div
+    class="font-kuro-mono flex flex-col lg:flex-row lg:items-center gap-y-0.5 gap-x-2 select-none"
+  >
+    {#each options as { href, key, label }}
+      <a
+        {href}
+        class={cn(
+          key === defaultVal ? "bg-kuro-lavender-700 text-white" : "",
+          "rounded-sm px-2 lg:px-1.5 py-1 lg:py-0.5",
+        )}
+        aria-current={key === defaultVal ? "page" : undefined}
+      >
+        {label}
+      </a>
+    {/each}
+  </div>
+</div>
