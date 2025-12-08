dfbaa22028c05abdf550cb2fec511499518ea245
Kuro <hello@kurojifusky.com>
2025-12-08 16:48:05 +0800

    i'm done
    squashing 10+ commits of getting the sanity deployment to work

--- sanity-studio/sanity.cli.ts
+++ sanity-studio/sanity.cli.ts
@@ -1,11 +1,15 @@
 import { defineCliConfig } from "sanity/cli"
 
 export default defineCliConfig({
+  project: {
+    basePath: "/studio"
+  },
   api: {
-    projectId: process.env.SANITY_STUDIO_PROJECT_ID,
-    dataset: process.env.SANITY_STUDIO_DATASET
+    projectId: process.env.SANITY_STUDIO_PROJECT_ID!,
+    dataset: process.env.SANITY_STUDIO_DATASET!
   },
   deployment: {
+    appId: process.env.SANITY_STUDIO_APPID!,
     autoUpdates: true
   }
 })
